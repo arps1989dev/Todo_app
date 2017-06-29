bd26090afb105e6cb5e4474121339b168896f3f1
bhaumikwork <bhaumikwork>
2017-06-29 14:59:17 +0530

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,6 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
-
   end
 
   def current_resource_owner
